6d07ae69f99d9bd3709bffa96b3d8be75a5fe8e0
Maksim Kultyshev <mksbnt@hotmail.com>
2019-09-22 22:45:11 +0300

    Create an React element

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log("It's empty");
+import React from "react";
+import ReactDOM from "react-dom";
+
+let el = (
+  <div>
+    <h1>todo list</h1>
+    <input placeholder="search" />
+    <ul>
+      <li>learn react</li>
+      <li>create app</li>
+    </ul>
+  </div>
+);
+
+ReactDOM.render(el, document.getElementById("root"));
